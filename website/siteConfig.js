9c27e4c88925cf98eb352441a49b6158bb3b1a31
mibensilalen <114273347+mibensilalen@users.noreply.github.com>
2022-11-13 23:24:06 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Mohamed Ibensilalen', // Title for your website.
     tagline: 'Technologist, awesome, and amazing.',
-    profilePhoto: 'img/profile.jpeg',
+    profilePhoto: 'img/profile.png',
     url: 'https://mibensilalen.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
